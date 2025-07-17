addf97fe8c24b2f4a64d2575e9c4ea459198e4b5
Brooke Morrison <bwookieeeee@gmail.com>
2025-07-17 15:30:45 -0700

    Add comment-based help

--- generic/Confirm-FileNames.ps1
+++ generic/Confirm-FileNames.ps1
@@ -1,4 +1,29 @@
+<#
+.SYNOPSIS
+    Confirms a list of file names matches against a regex string.
 
+.DESCRIPTION
+    Takes an array of FileInfo objects and compares the name against a user-
+    provided regex string. 
+
+.PARAMETER Files
+    Array of FileInfo objects to confirm file names against.
+
+.PARAMETER RegexString
+    Regex string to compare with $Files
+
+.INPUTS
+    FileInfo[]
+
+.OUTPUTS
+    FileInfo[] of all files failing the regex challenge
+
+.EXAMPLE
+    ./generic/Confirm-FileNames.ps1 -Files (Get-ChildItem -Path "./testfiles/*.*" -Recurse) -RegexString "((.\d+-\d+)|(UNKNOWN))_\d{2}( \(\d\))*\.(pdf|tif)"
+
+.EXAMPLE
+    Get-ChildItem -Path "./testfiles/*.*" -Recurse | ./generic/Confirm-FileNames.ps1 -RegexString "((.\d+-\d+)|(UNKNOWN))_\d{2}( \(\d\))*\.(pdf|tif)"
+#>
 
 $SourceDir = "./testfiles/"
 $regex = "((.\d+-\d+)|(UNKNOWN))_\d{2}( \(\d\))*\.(pdf|tif)"
@@ -7,7 +32,7 @@ $i = 0
 
 foreach ($file in $files) {
     Write-Verbose "Checking $($file.Name)"
-    if(!($file.Name -match $regex)) {
+    if (!($file.Name -match $regex)) {
         Write-Warning "Bad file name: $($file.FullName)"
         $i++
     }
