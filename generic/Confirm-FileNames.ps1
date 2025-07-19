8cff8bdc1c8a4269ebf9d3bb46cb71846cc32a83
Brooke Morrison <bwookieeeee@gmail.com>
2025-07-18 18:30:29 -0700

    Turn basic script into function

--- generic/Confirm-FileNames.ps1
+++ generic/Confirm-FileNames.ps1
@@ -25,17 +25,42 @@
     Get-ChildItem -Path "./testfiles/*.*" -Recurse | ./generic/Confirm-FileNames.ps1 -RegexString "((.\d+-\d+)|(UNKNOWN))_\d{2}( \(\d\))*\.(pdf|tif)"
 #>
 
-$SourceDir = "./testfiles/"
-$regex = "((.\d+-\d+)|(UNKNOWN))_\d{2}( \(\d\))*\.(pdf|tif)"
-$files = Get-ChildItem "$sourceDir*.*" -Recurse
-$i = 0
 
-foreach ($file in $files) {
-    Write-Verbose "Checking $($file.Name)"
-    if (!($file.Name -match $regex)) {
-        Write-Warning "Bad file name: $($file.FullName)"
-        $i++
+$BadFiles = @()
+
+function Confirm-FileNames {
+
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        $Files,
+        [Parameter(Mandatory = $true)]
+        [string]
+        $RegexString,
+        [switch]
+        $FullWidthOutput
+    )
+
+    foreach ($File in $Files) {
+
+        # I like full width output spacing because I can just scan my eyes down
+        # the terminal to quickly see what passes and fails. It's a switch 
+        # because I do not like this on extremely wide terminals.
+        $str = $File.Name + " "
+        if ($FullWidthOutput) {
+            $len = $str.Length
+            for ($i = 0; $i -lt $Host.UI.RawUI.WindowSize.Width - $len - 8; $i++) {
+                $str += "."
+            }
+        }
+        Write-Host -NoNewline $str
+        Write-Host -NoNewline -ForegroundColor DarkMagenta " ["
+        if ($file.Name -match $RegexString) {
+            Write-Host -NoNewline -ForegroundColor DarkGreen "PASS"
+        }
+        else {
+            Write-Host -NoNewline -ForegroundColor Red "FAIL"
+            $badFiles += $File.Name
+        }
+        Write-Host -ForegroundColor DarkMagenta "]"
     }
 }
-
-Write-Output "$i bad file names"
